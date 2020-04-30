0cb024638acba9a380a56292b83143fe309d7edc
Chibuisi Enyia <enyia21@gmail.com>
2020-04-30 22:28:42 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,13 +2,13 @@ class Backer
 
 
     attr_reader :name, :backed_projects
-  
+
     def initialize(name)
         @name = name
         @backed_projects = []
     end
 
-    def back_project(project)   
+    def back_project(project)
         @backed_projects << project
         project.add_backer(self)
 
